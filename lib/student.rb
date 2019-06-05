b3067be499447c7b6a27f63538b0b754149fd5fd
michaelstsnow <michael.st.snow@gmail.com>
2019-06-05 18:10:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,9 @@ class Student
   def self.new_from_db(row)
     # create a new Student object given a row from the database
     student=Student.new
-    student.id=row.id
-    student.name=
-    student.grade=
+    student.id=row[0]
+    student.name=row[1]
+    student.grade=row[2]
   end
 
   def self.all
