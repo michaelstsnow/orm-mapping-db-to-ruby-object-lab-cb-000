505b2c65813edd653e40906bf1273b99fb8f8831
michaelstsnow <michael.st.snow@gmail.com>
2019-06-05 18:23:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -114,8 +114,16 @@ class Student
     end
   end
 
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade_x)
+    sql=<<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    SQL
 
+    DB[:conn].execute(sql,grade_x).map do |row|
+      self.new_from_db(row)
+    end
   end
 
 end
